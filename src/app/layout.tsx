934fac22de09f6bcd5c4cc15bbc455cdb3b0e416
Wyatt Barnes <me@wyatt.email>
2024-03-19 17:40:49 -1000

    Replace adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4709633264763199" crossOrigin="anonymous"></script>
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,19 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.center}>
         Hello World
+
+        <iframe
+          data-aa='2311888'
+          src='//acceptable.a-ads.com/2311888'
+          style={{
+            border: 0,
+            padding: 0,
+            width: '100%',
+            height: '100%',
+            overflow: 'hidden',
+            backgroundColor: 'transparent',
+          }}
+        ></iframe>
       </div>
     </main>
   );
